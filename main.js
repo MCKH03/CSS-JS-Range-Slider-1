1b5bd3f0aa096f9318d329089de5b1fe14622f4d
Mehrshad Cheshm Khavari <ytmehrshad@gmail.com>
2023-08-24 11:57:55 +0330

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// Selecting individual elements
+const slider = document.querySelector(".slider");
+const progress = document.querySelector(".progress");
+const thumb = document.querySelector(".slider-thumb");
+const allNum = document.querySelectorAll(".num");
+
+// Functions
+const sliderFunc = (e) => {
+  const maxVal = slider.getAttribute("max");
+  const val = +slider.value;
+
+  allNum.forEach((num, i) => {
+    if (i * 10 <= val) {
+      num.classList.add("active");
+    } else {
+      num.classList.remove("active");
+    }
+  });
+
+  progress.style.width = val + "%";
+  thumb.style.left = val + "%";
+};
+
+// Event listeners
+slider.addEventListener("input", sliderFunc);
